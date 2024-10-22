20a50cefc437ffb8e363d0447a58af44e2212576
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-22 12:24:30 +0800

    refactor : button dart

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -10,10 +10,10 @@ class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(color: Color.fromARGB(212, 135, 81, 77)),
-      padding: EdgeInsets.all(9),
+      decoration: BoxDecoration(color: Color.fromARGB(212, 135, 81, 77), borderRadius: BorderRadius.circular(40)),
+      padding: EdgeInsets.all(10),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween, // Memberikan jarak antara text dan ikon
+        mainAxisAlignment: MainAxisAlignment.center, // Memberikan jarak antara text dan ikon
         children: [
           // text
           Text(
@@ -21,10 +21,13 @@ class MyButton extends StatelessWidget {
             style: TextStyle(color: Colors.white),
           ),
 
+          const SizedBox(
+            width: 10,
+          ),
           // icon
           Icon(
             Icons.arrow_forward,
-            color: Colors.white, // Menambahkan warna pada ikon
+            color: Colors.white,
           ),
         ],
       ),
