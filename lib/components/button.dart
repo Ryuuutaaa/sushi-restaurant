175b3f9bcd23ea2e824f164ba222a93832a1c67b
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-22 12:31:16 +0800

    feat : add ontab

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -2,34 +2,36 @@ import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
   final String text;
-  const MyButton({
-    super.key,
-    required this.text,
-  });
+  final void Function()? onTap;
+
+  const MyButton({super.key, required this.text, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: Color.fromARGB(212, 135, 81, 77), borderRadius: BorderRadius.circular(40)),
-      padding: EdgeInsets.all(10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center, // Memberikan jarak antara text dan ikon
-        children: [
-          // text
-          Text(
-            text,
-            style: TextStyle(color: Colors.white),
-          ),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(color: Color.fromARGB(212, 135, 81, 77), borderRadius: BorderRadius.circular(40)),
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center, // Memberikan jarak antara text dan ikon
+          children: [
+            // text
+            Text(
+              text,
+              style: TextStyle(color: Colors.white),
+            ),
 
-          const SizedBox(
-            width: 10,
-          ),
-          // icon
-          Icon(
-            Icons.arrow_forward,
-            color: Colors.white,
-          ),
-        ],
+            const SizedBox(
+              width: 10,
+            ),
+            // icon
+            Icon(
+              Icons.arrow_forward,
+              color: Colors.white,
+            ),
+          ],
+        ),
       ),
     );
   }
