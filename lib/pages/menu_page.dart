1ada83f8b390f516dc0f3527a2040815aa815381
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-23 14:33:32 +0800

    feat : add padding popular food

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -138,6 +138,7 @@ class _MenuPageState extends State<MenuPage> {
           // popular food
           Container(
             decoration: BoxDecoration(color: Colors.grey[100], borderRadius: BorderRadius.circular(20)),
+            margin: const EdgeInsets.only(left: 25, right: 25, bottom: 25),
             child: Row(
               children: [
                 // image
