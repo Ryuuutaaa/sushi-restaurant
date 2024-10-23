3336d18871110a6a396aa67020bb5b97ccf492d6
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-23 13:49:37 +0800

    feat : padding and scroll horizontal

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -122,10 +122,14 @@ class _MenuPageState extends State<MenuPage> {
           ),
 
           Expanded(
-            child: ListView.builder(
-              itemCount: foodMenu.length,
-              itemBuilder: (context, index) => FoodTile(
-                food: foodMenu[index],
+            child: Padding(
+              padding: const EdgeInsets.only(left: 25.0),
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: foodMenu.length,
+                itemBuilder: (context, index) => FoodTile(
+                  food: foodMenu[index],
+                ),
               ),
             ),
           )
