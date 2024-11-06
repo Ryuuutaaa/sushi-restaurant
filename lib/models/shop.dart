8bb4d8ca7894d82716667188a02e9b2dd04a5b46
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-11-07 01:37:13 +0800

    feat : add getter methods

--- lib/models/shop.dart
+++ lib/models/shop.dart
@@ -2,7 +2,7 @@ import 'package:sushi/models/food.dart';
 
 class Shop {
   // food  menu
-  final List<Food> foodMenu = [
+  final List<Food> _foodMenu = [
     // salmon suhsi
     Food(name: "Salmon Sushi", price: "21.00", imagePath: "lib/images/salmon_sushi.png", rating: "4.9"),
 
@@ -12,4 +12,8 @@ class Shop {
 
   // customer card
   List<Food> _cart = [];
+
+  // getter  methods
+  List<Food> get foodMenu => _foodMenu;
+  List<Food> get cart => _cart;
 }
