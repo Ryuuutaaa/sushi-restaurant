49025965b4749b1d0a0810c4594288c82a10215e
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-11-07 18:14:01 +0800

    feat : get started:

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:sushi/theme/colors.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "My cart",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: primaryColor,
+      ),
+    );
   }
 }
